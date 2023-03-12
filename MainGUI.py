ec766065ce509c2b4c9e1347f092b2cad6070c6d
tomohirok8 <tomohiro.k8@gmail.com>
2023-03-12 23:33:57 +0900

    GUI更新

--- MainGUI.py
+++ MainGUI.py
@@ -2,10 +2,10 @@ import os
 import sys
 import tkinter as tk
 from tkinter import filedialog, ttk, messagebox
-import configparser, webbrowser
-import math
+import configparser
 import torch
 import pandas as pd
+
 
 class TextEdit:
     def __init__(self, root):
@@ -28,13 +28,15 @@ class TextEdit:
         
         # ハンドラ関数
         def get_entry1():
-            messagebox.showinfo('メッセージ', entry1.get())
+            self.label6.configure(text=entry1.get())
+            messagebox.showinfo('メッセージ', text=self.label6)
         
         def get_combo1():
             messagebox.showinfo('メッセージ', combo1.get())
         
         def get_radio1():
             messagebox.showinfo('メッセージ', action[var.get()])
+        
         
         def count_up():
             # global count
@@ -48,6 +50,10 @@ class TextEdit:
         label2 = tk.Label(root, text='データ読み込み', foreground='red')
         self.label3 = tk.Label(root, text='trainデータパス', foreground='red')
         self.label4 = tk.Label(root, text='testデータパス', foreground='red')
+        label5 = tk.Label(root, text='目的変数読み込み', foreground='red')
+        self.label6 = tk.Label(root, text='目的変数', foreground='red')
+        label7 = tk.Label(root, text='結果格納フォルダ選択', foreground='red')
+        self.label8 = tk.Label(root, text='結果フォルダパス', foreground='red')
         
         
         # Entryウィジェットの生成
@@ -57,8 +63,8 @@ class TextEdit:
         button1 = tk.Button(root, text='Pytorch環境', command=self.check_pytorch)
         button2 = tk.Button(root, text='trainデータ読み込み', command=self.trainFileOpen)
         button3 = tk.Button(root, text='testデータ読み込み', command=self.testFileOpen)
-        # button1 = tk.Button(root, text='ボタン1', command=count_up)
-        # button2 = tk.Button(root, text='表示', command=get_entry1)
+        button4 = tk.Button(root, text='目的変数読み込み', command=get_entry1)
+        button5 = tk.Button(root, text='結果格納フォルダ選択', command=self.resultFolderOpen)
         button98 = tk.Button(root, text='コンボ', command=get_combo1)
         button99 = tk.Button(root, text='コンボ', command=get_radio1)
         
@@ -66,7 +72,7 @@ class TextEdit:
         combo1 = ttk.Combobox(root, state='readonly', values=weather)
         
         
-
+        # Radiobuttonウィジェットの生成
         action = ['選択肢1', '選択肢2', '選択肢3', '選択肢4']
         var = tk.IntVar(value=0)
         radio1 = tk.Radiobutton(root, text=action[0], variable=var, value=0)
@@ -74,25 +80,29 @@ class TextEdit:
         radio3 = tk.Radiobutton(root, text=action[2], variable=var, value=2)
         radio4 = tk.Radiobutton(root, text=action[3], variable=var, value=3)
 
-        # Buttonウィジェットの生成と配置
-        # btn4 = tk.Button(root, text='表示', command=click_get)
-
         # grid関数でウィジェットを配置
-        label1.grid(row=0, column=0, sticky=tk.E)
-        button1.grid(row=0, column=1, sticky=tk.E)
-        label2.grid(row=1, column=0, sticky=tk.N)
-        button2.grid(row=1, column=1, sticky=tk.S)
-        self.label3.grid(row=1, column=2, sticky=tk.S)
-        button3.grid(row=2, column=1, sticky=tk.S)
-        self.label4.grid(row=2, column=2, sticky=tk.S)
+        label1.grid(row=0, column=0, sticky=tk.W)
+        button1.grid(row=0, column=1, sticky=tk.W)
+        label2.grid(row=1, column=0, sticky=tk.W)
+        button2.grid(row=1, column=1, sticky=tk.W)
+        self.label3.grid(row=1, column=3, sticky=tk.W)
+        button3.grid(row=2, column=1, sticky=tk.W)
+        self.label4.grid(row=2, column=3, sticky=tk.W)
+        label5.grid(row=3, column=0, sticky=tk.W)
+        entry1.grid(row=3, column=1, sticky=tk.W)
+        button4.grid(row=3, column=2, sticky=tk.W)
+        self.label6.grid(row=3, column=3, sticky=tk.W)
+        label7.grid(row=4, column=0, sticky=tk.W)
+        button5.grid(row=4, column=2, sticky=tk.W)
+        self.label8.grid(row=4, column=3, sticky=tk.W)
         combo1.grid(row=5, column=1, sticky=tk.W)
-        button98.grid(row=5, column=2, sticky=tk.NSEW)
+        button98.grid(row=5, column=2, sticky=tk.W)
         radio1.grid(row=6, column=0)
         radio2.grid(row=6, column=1)
         radio3.grid(row=6, column=2)
         radio4.grid(row=6, column=3)
-        button99.grid(row=7, column=3, sticky=tk.NSEW)
-        # entry1.grid(row=1, column=1, sticky=tk.W)
+        button99.grid(row=7, column=3, sticky=tk.W)
+        
         # button1.grid(row=1, column=2, sticky=tk.E)
         # button2.grid(row=1, column=3, sticky=tk.NSEW)
         
@@ -134,7 +144,8 @@ class TextEdit:
         s += '©2023 Tomohiro K\n'
         s += 'with Python ' + sys.version
         messagebox.showinfo(self.__class__.__name__, s)
-        
+
+    
     def menuFileExit(self):
         cp = configparser.ConfigParser()
         cp['Client'] = {
@@ -145,7 +156,8 @@ class TextEdit:
         with open(self.__class__.__name__ + '.ini', 'w') as f:
             cp.write(f)
         root.destroy()
-    
+
+
     def check_pytorch(self):
         cpt = 'GPU available : ' + str(torch.cuda.is_available()) + '\n'
         cpt += 'Pytorch version : ' +  torch.__version__ + '\n'
@@ -154,6 +166,7 @@ class TextEdit:
         cpt += 'GPU device name : ' + torch.cuda.get_device_name() + '\n'
         cpt += 'GPU device capability : ' + str(torch.cuda.get_device_capability())
         messagebox.showinfo('Pytorch環境', cpt)
+
 
     def trainFileOpen(self):
         try:
@@ -164,13 +177,13 @@ class TextEdit:
         try:
             train_data = pd.read_csv(filepath)
             self.trainFile = train_data # csvファイルの中身
-            self.trainFilePath = filepath
+            # self.trainFilePath = filepath
             self.directory = filepath
         except:
             messagebox.showwarning(self.__class__.__name__, 'ファイルを開けませんでした')
-        
         self.label3.configure(text=filepath)
-    
+
+
     def testFileOpen(self):
         try:
             filepath = filedialog.askopenfilename(filetypes=self.fileTypes, initialdir=self.directory)
@@ -178,21 +191,42 @@ class TextEdit:
             filepath = ''
         
         try:
-            train_data = pd.read_csv(filepath)
-            self.testnFile = train_data # csvファイルの中身
-            self.testFilePath = filepath
+            test_data = pd.read_csv(filepath)
+            self.testFile = test_data # csvファイルの中身
+            # self.testFilePath = filepath
             self.directory = filepath
         except:
             messagebox.showwarning(self.__class__.__name__, 'ファイルを開けませんでした')
-        
         self.label4.configure(text=filepath)
-        
+
+
+    def resultFolderOpen(self):
+        try:
+            folderpath = filedialog.askdirectory(initialdir=self.directory)
+        except:
+            folderpath = ''
+        self.resultFolder = folderpath
+        self.label8.configure(text=folderpath)
+
+    
+    def readdata(self):
+        return self.trainFile, self.testFile, self.resultFolder
 
 
 root = tk.Tk()
-TextEdit(root)
+gui = TextEdit(root)
+# root.minsize(800, 600)
 # print(root.children)
 root.mainloop()
+
+# print(vars(root))
+
+train_data, test_data, savedir = gui.readdata()
+
+
+
+
+
 
 
 
