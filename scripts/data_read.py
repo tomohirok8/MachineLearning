bea6025fb3c42102bf6821967ff3c36bb4e9a894
tomohirok8 <tomohiro_k8@gmail.com>
2023-08-14 15:27:33 +0900

    スクリプトの構成変更

--- scripts/data_read.py
+++ scripts/data_read.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.datasets import load_iris
 
 
 
@@ -6,14 +7,22 @@ class My_Data_Read:
     def SIG_mpg():
         train_data = pd.read_table("data/SIGNATE_exercises_mpg/train.tsv")
         test_data = pd.read_table("data/SIGNATE_exercises_mpg/test.tsv")
-        
         return train_data, test_data
     
     def RedWineQuality():
         train_data = pd.read_csv('data/RedWineQuality/train.csv')
         test_data = pd.read_csv('data/RedWineQuality/test.csv')
-        
         return train_data, test_data
+    
+    def Iris():
+        dataset = load_iris()
+        x = pd.DataFrame(dataset.data,columns=dataset.feature_names)
+        y = pd.Series(dataset.target, name='Variety')
+        print('----------------------------------------')
+        print('df_iris shape: (%i,%i)' %X.join(y).shape)
+        print('-----------------------------------------')
+        display(x.join(y).head(5))()
+        return x, y
 
 
 
